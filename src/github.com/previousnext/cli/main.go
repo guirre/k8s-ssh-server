9472133f495cbd91708610437adf588e1396695e
Nick Schuch <nick@myschuch.com>
2017-04-16 18:24:37 +1000

    Adds CLI

--- src/github.com/previousnext/cli/main.go
+++ src/github.com/previousnext/cli/main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/gosuri/uitable"
+	"k8s.io/client-go/rest"
+
+	sshclient "github.com/previousnext/client"
+)
+
 func main() {
-	// Coming soon...
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	sshClient, err := sshclient.NewClient(config)
+	if err != nil {
+		panic(err)
+	}
+
+	list, err := sshClient.ListAll()
+	if err != nil {
+		panic(err)
+	}
+
+	table := uitable.New()
+	table.MaxColWidth = 80
+
+	table.AddRow("NAMESPACE", "NAME", "KEY")
+	for _, key := range list.Items {
+		table.AddRow(key.Metadata.Namespace, key.Metadata.Name, key.Spec.AuthorizedKey)
+	}
+	fmt.Println(table)
 }
